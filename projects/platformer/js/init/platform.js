31f130447c727752677155dcb7893c4474c4d9fd
JustinGeorge179 <noobsquad179@gmail.com>
2021-06-21 16:54:21 +0000

    box moves

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -38,9 +38,9 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
 
         // example:
-        createPlatform(400, 250);
-        createPlatform(170, 460, 0.5);
-        
+        createPlatform(100, 400,1);
+        createPlatform(100, 500, 1.5);
+        createPlatform(150,300)
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
